15f2aba970fae8761d3d61ec1295ae5181cbaeb9
Amanda Hernández <amandavhernan@gmail.com>
2023-05-17 05:05:47 +0000

    blog post

--- plotly.py
+++ plotly.py
@@ -1,34 +1,7 @@
-import requests
-import json
-import time
-import os
 import pandas as pd
 import plotly.graph_objs as go
-import plotly.io as pio
-from slack import WebClient
-from slack.errors import SlackApiError
-from datetime import datetime
 
 def top10chart():
-
-    # api endpoint
-    COMPLAINTS_API_ENDPOINT = "https://opendata.maryland.gov/resource/cnkn-n3pr.json"
-
-    try:
-        with open('last_checked_date.txt', 'r') as f:
-            last_checked_date = f.read().strip()
-    except FileNotFoundError:
-        with open('last_checked_date.txt', 'w') as f:
-            last_checked_date = '2023-03-30'  # default date if file doesn't exist
-            f.write(last_checked_date)
-
-    # store api response, check for new complaints
-    response = requests.get(COMPLAINTS_API_ENDPOINT)
-    complaints_data = json.loads(response.content)
-
-    # save as pandas df, export as csv
-    df = pd.DataFrame(complaints_data)
-    df.to_csv("complaints_data.csv", index=False)
 
     # complaints by county
     complaints = pd.read_csv('complaints_data.csv')
